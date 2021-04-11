a05cd2ed009e68c039a31bf55a1e839a1b1a680b
xiaochuan <55593153+xiaochuan-li@users.noreply.github.com>
2021-04-11 23:21:19 +0800

    Update MotionCorrection.py
    1. 优化代码逻辑，避免重复打开同一个文件/避免临时文件生成
    2. 添加优化算法：指数平滑，低通滤波
    3. 添加可视化分析函数anal
    4. 添加优化算法组合功能

--- MotionCorrect/MotionCorrection.py
+++ MotionCorrect/MotionCorrection.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-'''
+"""
 @File    :   MotionCorrection.py
 @Time    :   2021/04/11 18:24:48
 @Author  :   Xiaochuan LI  Chunyu Deng Jiaxin Li
@@ -7,7 +7,7 @@
 @Contact :   lixiaochuan@buua.edu.cn
 @License :   (C)Copyright 2020-2021, Lixiaochuan-BUAA-vrlab
 @Desc    :   None
-'''
+"""
 
 
 import numpy as np
@@ -17,146 +17,107 @@ from scipy import signal
 import linecache
 import re
 import argparse
+import matplotlib.pyplot as plt
 
-def BVHtoDataFrame(filename):               #将BVH数据解析成DataFrame，将节点的角度和名称对应起来
-    node_list=[]
-    node_append_channel=[]
 
-    f=open(filename)    #your path!
-
-    for line in f:
-        words=line.split()
-        for word in words:
-            if(word=='ROOT'):
-                node_list.append(words[1])
-            if(word=='JOINT'):
-                node_list.append(words[1])
-            
-    
-    f.close()
-
-    index_node=0
-
-    f=open(filename)    #your path!
-    for line in f:
-        words=line.split()
-        for word in words:
-            if(word=='CHANNELS'):
-                for i in range(2,2+int(words[1])):
-                    node_append_channel.append(node_list[index_node]+'-'+words[i])
-                index_node+=1
-
-    f.close()
+def BVHtoDataFrame(filename):  # 将BVH数据解析成DataFrame，将节点的角度和名称对应起来
+    node_list = []
+    node_append_channel = []
 
     f = open(filename)
-    flag = 0
-    count = 0
-    number_line_final = []
-    for line in f:
-        eachline = line.split()
-        if(eachline[0] == 'Frame'):
-            flag = 1
-            continue
-        if(flag):
-            count += 1
-            number_line = map(float, eachline)
-            number_line = list(number_line)
-            if(count!=1):
-                number_line_final = np.vstack((number_line_final,number_line))
+    namePTN = re.compile("(ROOT|JOINT) (.*)")
+    channelPTN = re.compile("CHANNELS \d (.*)")
+    matrixPTN = re.compile("([\d|\.|\s|-]{1,})")
+
+    tmpName = ""
+    colName = []
+    angData = []
+    header = ""
+    with open(filename, "r") as fin:
+        for line in fin.readlines():
+            simLine = line.strip()
+            if matrixPTN.match(simLine):
+                angData.append(list(map(float, simLine.split(" "))))
             else:
-                number_line_final = number_line
-    df = pd.DataFrame(data = number_line_final, columns=node_append_channel)
+                header += line
+                if namePTN.match(simLine):
+                    tmpName = namePTN.match(simLine).group(2)
+                elif channelPTN.match(simLine):
+                    channels = channelPTN.match(simLine).group(1).split(" ")
+                    colName += ["{}_{}".format(tmpName, x) for x in channels]
+                else:
+                    continue
+    df = pd.DataFrame(data=np.array(angData), columns=colName)
+    return df, header
 
-    return df
 
-def BVHtoCSV(filename,targetname):              #将已经解析好的DataFrame存到csv文件中
-    df = BVHtoDataFrame(filename)
-    df.to_csv(targetname+".csv")
+def BVHtoCSV(filename, targetname):  # 将已经解析好的DataFrame存到csv文件中
+    df, _ = BVHtoDataFrame(filename)
+    df.to_csv(targetname + ".csv")
 
-def JointAllAngle(filename,jointname,axis):     #获得BVH文件中某一结点的某一方向的全部角度值         
-    df = BVHtoDataFrame(filename)               #axis   0代表x轴，1代表y轴，2代表z轴
-    if(axis==0):
-        column_name = jointname+'-Xrotation'
-    elif(axis==1):
-        column_name = jointname+'-Yrotation'
-    elif(axis==2):
-        column_name = jointname+'-Zrotation'
-    else:
-        print('axis error')
-        return
-    res = df[column_name].values
-    return res
 
-def mergeTxt(file1, file2):                     #将file2连接的file1文件的末尾
-    f1 = open(file1, 'a+', encoding='utf-8')
-    with open(file2, 'r', encoding='utf-8') as f2:
-        f1.write('\n')
-        for i in f2:
-            f1.write(i)
+def exponentialSmoothing(data, alpha=0.8):
+    data = data.reshape(data.shape[0], -1)
+    S = data.copy()
+    S[0] = np.mean(S, axis=0)
+    for i in range(1, data.shape[0]):
+        S[i] = S[i - 1] * alpha + S[i] * (1 - alpha)
+    return S
 
-def SaveBvhHead(input_filename):                #将BVH文件的数据结构部分提取出来
-    
-    with open(input_filename) as f:       #修改的目标文件位置
-        num_total = len(f.readlines())
-        num_total = int(num_total) 
-    f.close()
-    f=open(input_filename)    #your path!
-    for line in f:
-        words=line.split()
-        for word in words:
-            if(word=='Frames:'):
-                num_frames=int(words[1])          #提取frame  
-    f.close()
-    Num_before_End=num_total-num_frames+1
-    res_line=[]
-    
-    for line_frame2 in range(0,Num_before_End-1):
-        the_line_frame2 = linecache.getline(input_filename, line_frame2)
-        res_line.append(the_line_frame2)
 
-    #干掉最后一行的换行符
-    the_line_finall= linecache.getline(input_filename, Num_before_End-1)
-    words1=the_line_finall.split()
+def lowPassSmoothing(data, deg=8, ratio=0.01):
+    data = data.reshape(data.shape[0], -1)
+    b, a = signal.butter(deg, ratio, "lowpass")
+    for i in range(data.shape[1]):
+        data[:, i] = signal.filtfilt(b, a, data[:, i])
+    return data
 
-    line_F=""
-    for word2 in words1:
-        line_F+=word2
-        line_F+=' '
-        
-    res_line.append(line_F)
-    
-    fileObject = open('process.bvh', 'w')
-    for ip2 in res_line:
-        fileObject.write(ip2)
-    fileObject.close()
 
-def exponentialSmoothing(filename,jointname,axis):
-    res = JointAllAngle(filename, jointname,axis)
-    final = []
-    for num in res:
-        num = 0.0001*np.random.rand()
-        final.append(num)
-    df = BVHtoDataFrame(filename)
-    if(axis==0):
-        df[jointname+'-Xrotation'] = final
-    elif(axis==1):
-        df[jointname+'-Yrotation'] = final
-    elif(axis==2):
-        df[jointname+'-Zrotation'] = final
-    else:
-        print('axis error')
-        return
-    #df.to_csv('result.csv')
-    np.savetxt(r'process_tail.bvh', df.values, fmt='%f')
-    SaveBvhHead(filename)
-    mergeTxt('process.bvh', 'process_tail.bvh')
-    os.remove('process_tail.bvh')
+def zerolizeSmoothing(data, factor=0.0001):
+    size = data.shape
+    randarr = factor * np.random.randn(*size)
+    return randarr.reshape(size[0], -1)
 
-def downSmpling(oripath, trgPath, t=12):
+
+def processSmoothing(filename, jointNames, methods="el"):
+    channels = ["Xrotation", "Yrotation", "Zrotation"]
+    processCols = []
+    for jointName in jointNames:
+        processCols += ["{}_{}".format(jointName, channel) for channel in channels]
+    df, header = BVHtoDataFrame(filename)
+    smootherDict = {
+        "e": exponentialSmoothing,
+        "l": lowPassSmoothing,
+        "z": zerolizeSmoothing,
+    }
+    assert all(
+        [method in smootherDict for method in methods]
+    ), "Smooth Option Not Valide"
+    processedData = df[processCols].values
+    for method in methods:
+        processedData = smootherDict[method](processedData)
+    # anal(ori=df[processCols].values, trg=processedData, title=methods)     # 可视化分析
+    df[processCols] = processedData
+    dataStr = "\n".join([" ".join(map(str, x)) for x in df.values])
+    return header + dataStr
+
+
+def anal(ori, trg, title="title"):
+    plt.figure()
+    for i in range(ori.shape[1]):
+        plt.subplot(ori.shape[1], 1, i + 1)
+        plt.title(title)
+        plt.plot(np.arange(ori.shape[0]), ori[:, i], label="ori")
+        plt.plot(np.arange(trg.shape[0]), trg[:, i], label="trg")
+        plt.legend()
+    plt.show()
+
+
+def downSmpling(bvhStr, trgPath, t=12):
     """对bvh文件等距下采样，实现抽帧的效果
 
     Args:
-        oripath (str): bvh源文件路径
+        bvhStr (str): bvh源文件字符串
         trgPath (str): bvh目标文件路径
         t (int, optional): 采样率. Defaults to 12.
     """
@@ -164,29 +125,29 @@ def downSmpling(oripath, trgPath, t=12):
     f_ptn = re.compile("Frames: (.*)")
     flag = False
     count = 0
-    with open(oripath, "r") as fin:
-        with open(trgPath, "w") as fout:
-            lines = fin.readlines()
-            for line in lines:
-                res = f_ptn.match(line)
-                if res:
-                    f = str(int(res.group(1)) // t)
-                    fout.write("Frames: {}\n".format(f))
-                    continue
-                res = ptn.match(line)
-                if res:
-                    time = str(float(res.group(1)) * t)
-                    fout.write("Frame Time: {}\n".format(time))
-                    flag = True
+    with open(trgPath, "w") as fout:
+        lines = bvhStr.split("\n")
+        for line in lines:
+            res = f_ptn.match(line)
+            if res:
+                f = str(int(res.group(1)) // t)
+                fout.write("Frames: {}\n".format(f))
+                continue
+            res = ptn.match(line)
+            if res:
+                time = str(float(res.group(1)) * t)
+                fout.write("Frame Time: {}\n".format(time))
+                flag = True
+            else:
+                if flag:
+                    if count % t == 0:
+                        fout.write(line + "\n")
+                    count += 1
                 else:
-                    if flag:
-                        if count % t == 0:
-                            fout.write(line)
-                        count += 1
-                    else:
-                        fout.write(line)
+                    fout.write(line + "\n")
 
-def MotionCorrection(oripath,trgPath,jointname,t):
+
+def MotionCorrection(oripath, trgPath, jointNames, t=12, methods="el"):
     """
     对指定关节的动作进行滤波（去抖动）
     对bvh文件等距下采样，实现抽帧的效果
@@ -194,31 +155,53 @@ def MotionCorrection(oripath,trgPath,jointname,t):
     Args:
         oripath (str): bvh源文件路径
         trgPath (str): bvh目标文件路径
-        jointname (list): bvh文件中希望更改的节点名的列表
+        jointNames (list): bvh文件中希望更改的节点名的列表
         t (int, optional): 采样率. Defaults to 12.
+        method (str): 优化方法，可选项{e: 指数平滑, l:低通滤波, z:置零}
     """
-    i = 0
-    for name in jointname:
-        i+=1
-        if(i==1):
-            exponentialSmoothing(oripath, name, 0)
-        else:
-            exponentialSmoothing('process.bvh', name, 0)
+    correctStr = processSmoothing(oripath, jointNames, methods=methods)
+    downSmpling(correctStr, trgPath)
 
-    downSmpling('process.bvh', trgPath)
 
-#脚本内功能测试
+def test():
+    MotionCorrection(
+        "data/get.bvh", "test_exp_01.bvh", ["RightElbow", "LeftElbow"], methods="e"
+    )
+    MotionCorrection(
+        "data/get.bvh", "test_low_0001_8.bvh", ["RightElbow", "LeftElbow"], methods="l"
+    )
+    MotionCorrection(
+        "data/get.bvh", "test_zero.bvh", ["RightElbow", "LeftElbow"], methods="el"
+    )
+
+
+# 脚本内功能测试
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--input", required=True,             
-	    help="path to input BVH file")                          # -i bvh源文件路径
-    ap.add_argument("-o", "--output", required=True,            
-	    help="path to output BVH file")                         # -o bvh目标文件路径
-    ap.add_argument('--names', nargs='+',                       
-        help="nodes need to fix")                               # --names bvh文件希望更改的节点名的列表
-    ap.add_argument("-f", "--frequency", required=True,type=int,   
-	    help="frequency of downsampling")                       # -f 采样频率
+    ap.add_argument(
+        "-i", "--input", required=True, help="path to input BVH file"
+    )  # -i bvh源文件路径
+    ap.add_argument(
+        "-o", "--output", required=True, help="path to output BVH file"
+    )  # -o bvh目标文件路径
+    ap.add_argument(
+        "--names", nargs="+", help="nodes need to fix"
+    )  # --names bvh文件希望更改的节点名的列表
+    ap.add_argument(
+        "-m",
+        "--methods",
+        required=False,
+        default="el",
+        help="optimize method",
+    )  # -m 优化方法组合，可选项（e/l/z），默认zl
+    ap.add_argument(
+        "-f", "--frequency", required=True, type=int, help="frequency of downsampling"
+    )  # -f 采样频率
     args = vars(ap.parse_args())
-    MotionCorrection(args["input"], args["output"], args["names"], args["frequency"])
-    os.remove('process.bvh')
-
+    MotionCorrection(
+        args["input"],
+        args["output"],
+        args["names"],
+        args["frequency"],
+        methods=args["methods"],
+    )
